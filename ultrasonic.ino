e58ae2d1ee88c8cc399a1b1f5adebe35440a56dc
Mohamad Idrus <mohamadidrus240@gmail.com>
2022-12-26 19:23:44 +0700

    Create ultrasonic.ino

---
+++ ultrasonic.ino
@@ -0,0 +1,22 @@
+//pin Ultrasonik
+#define trigPin 5
+#define echoPin 6
+
+void setup() {
+  Serial.begin (9600); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  }
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  delay(50);
+  Serial.println(distance);
+}
